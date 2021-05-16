3d877c9c105316e5eac632a3715876f39d455675
Bruno Dalcio Casas <bruno.casas2@gmail.com>
2021-05-16 12:55:32 -0300

    Reset project

--- back/db/db.js
+++
@@ -1,9 +0,0 @@
-var Pool = require('pg').Pool
-
-module.exports = new Pool({
-    user: 'master',
-    host: 'localhost',
-    database: 'balada_intergalatica',
-    password: 'masterpass',
-    port: 5432,
-});
